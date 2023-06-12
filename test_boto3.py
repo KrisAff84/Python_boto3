ac67c0c2547cea34a207a454dc8c27f73801bfd9
KrisAff84 <krisaff@gmail.com>
2023-06-12 18:16:38 +0000

    Created first boto3 scripts

--- test_boto3.py
+++ test_boto3.py
@@ -1,1 +1,13 @@
 import boto3
+
+session = boto3.Session()
+
+s3 = session.client('s3')
+s3 = boto3.client('s3')
+
+s3 = boto3.resource('s3')
+s3 = session.resource('s3')
+
+# Difference is level of extraction
+# Client gives more API type response
+# Resource provides python type response
