68cca9d3e6739f041afdaf5851bde12fe91de037
KrisAff84 <krisaff@gmail.com>
2023-06-14 05:49:09 +0000

    batch_write.py

--- DynamoDB/batch_write.py
+++ DynamoDB/batch_write.py
@@ -1,62 +1,43 @@
-response = client.batch_write_item(
-    RequestItems={
-        'string': [
-            {
-                'PutRequest': {
-                    'Item': {
-                        'string': {
-                            'S': 'string',
-                            'N': 'string',
-                            'B': b'bytes',
-                            'SS': [
-                                'string',
-                            ],
-                            'NS': [
-                                'string',
-                            ],
-                            'BS': [
-                                b'bytes',
-                            ],
-                            'M': {
-                                'string': {'... recursive ...'}
-                            },
-                            'L': [
-                                {'... recursive ...'},
-                            ],
-                            'NULL': True|False,
-                            'BOOL': True|False
+import boto3
+
+
+# Needs to be edited
+def batch_write():
+    ddb = boto3.client('dynamodb')
+    response = dbb.batch_write_item(
+        RequestItems={
+            'string': [
+                {
+                    'PutRequest': {
+                        'Item': {
+                            'string': {
+                                'S': 'string',
+                                'N': 'string',
+                                'B': b'bytes',
+                                'SS': [
+                                    'string',
+                                ],
+                                'NS': [
+                                    'string',
+                                ],
+                                'BS': [
+                                    b'bytes',
+                                ],
+                                'M': {
+                                    'string': {'... recursive ...'}
+                                },
+                                'L': [
+                                    {'... recursive ...'},
+                                ],
+                                'NULL': True|False,
+                                'BOOL': True|False
+                            }
                         }
-                    }
+                    },
+  
                 },
-                'DeleteRequest': {
-                    'Key': {
-                        'string': {
-                            'S': 'string',
-                            'N': 'string',
-                            'B': b'bytes',
-                            'SS': [
-                                'string',
-                            ],
-                            'NS': [
-                                'string',
-                            ],
-                            'BS': [
-                                b'bytes',
-                            ],
-                            'M': {
-                                'string': {'... recursive ...'}
-                            },
-                            'L': [
-                                {'... recursive ...'},
-                            ],
-                            'NULL': True|False,
-                            'BOOL': True|False
-                        }
-                    }
-                }
-            },
-        ]
-    },
-    ReturnConsumedCapacity='INDEXES'|'TOTAL'|'NONE',
-    ReturnItemCollectionMetrics='SIZE'|'NONE'
+            ]
+        },
+        ReturnConsumedCapacity='INDEXES'|'TOTAL'|'NONE',
+        ReturnItemCollectionMetrics='SIZE'|'NONE'
 )
