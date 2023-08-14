eb791ab8aa6dff102144a8dade17ee8e842fa750
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2023-08-14 10:26:07 -0500

    Delete gen_presignedURL.py

--- gen_presignedURL.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-s3 = boto3.client('s3')
-
-response = s3.generate_presigned_url('get_object', \
-Params={'Bucket': 'kris-boto3-0834287', 'Key': 'test_text_string.txt'}, \
-ExpiresIn=600)
-        
-print(response)
